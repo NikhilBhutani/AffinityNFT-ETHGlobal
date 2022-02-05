f605aaccccebf4afe24249250aa50a4ac37abc0f
NikhilBhutani <niks.bhutani@gmail.com>
2022-02-06 00:48:55 +0530

    Modified for RewardNFT

--- contracts/RewardNFT.sol
+++ contracts/RewardNFT.sol
@@ -1,44 +1,44 @@
-//SPDX License Identifier: UMLICENSED
-
 pragma solidity ^0.8.0;
-
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract RewardNFT is ERC721, Ownable {
+contract RewardNFT is ERC721URIStorage, Ownable {
+    using SafeMath for uint256;
+    using Counters for Counters.Counter;
+    using Strings for uint256;
 
-   using SafeMath for uint256;
+   string public constant tokenName = "AffinityRewardToken";
+   string public constant tokenSymbol = "ART";
 
-   string public constant tokenName = "RewardToken";
-   string public constant tokenSymbol = "RT";
-
-    uint256 tokenId;
+   Counters.Counter private tokenId;
 
     mapping(uint256 => address) ownerOfToken;
-    mapping(address => uint256) balance;
-
+    mapping(address => string) tokenURIOfOwner;
     
+    constructor() ERC721(tokenName, tokenSymbol) {
 
-    constructor() ERC721(tokenName, tokenSymbol){
     }
 
-    function balanceOf(address _owner) override public view returns (uint256){
-        return balance[_owner];
+    function mintToken(string memory _tokenURI) public returns (uint256) {
+        tokenId.increment();
+        console.log('Token URI ',_tokenURI);
+        console.log('Token ID ', tokenId.current());
+        uint256 newTokenId = tokenId.current();
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, _tokenURI);
+        tokenURIOfOwner[msg.sender] = _tokenURI;
+        return newTokenId;
+    }
+
+    function getTokenURI(address _user) public view returns (string memory){
+        return tokenURIOfOwner[_user];
     }
 
     function transfer(address to, uint256 _tokenId) public {
-        require(_own(msg.sender, _tokenId), "Only the creator can send RewardNFT's from this contract");
         transferFrom(msg.sender, to, _tokenId);
-    }
-
-    /** 
-    * @dev Function _own: This function checks the owner of each tokenId,
-    * Added this for requirement statements
-    */
-    function _own(address _claimant, uint256 _tokenId) private view returns(bool){
-        require(ownerOfToken[_tokenId] == _claimant);
-        return true;
     }
 
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,21 @@
 const hre = require("hardhat");
 
 async function main() {
-    const [deployer] = await hre.ethers.getSigners();
+    await deployCreatorNFT();
+    await deployRewardNFT();
+}
 
+async function deployCreatorNFT(){
     const CreatorNfT = await hre.ethers.getContractFactory("CreatorNFT");
     const creatorNFTContract = await CreatorNfT.deploy();
 
     await creatorNFTContract.deployed();
     console.log("Creator NFT contract address:", creatorNFTContract.address);
 
-    saveFrontendFiles(creatorNFTContract);
+    saveCreatorFEFiles(creatorNFTContract);
 }
 
-function saveFrontendFiles(contract) {
+function saveCreatorFEFiles(contract) {
     const fs = require("fs");
     const contractsDir = __dirname + "/../client/src/abi";
 
@@ -30,6 +33,37 @@ function saveFrontendFiles(contract) {
     fs.writeFileSync(
         contractsDir + "/CreatorNFT.json",
         JSON.stringify(CreatorNFTArtifact, null, 2)
+    );
+}
+
+async function deployRewardNFT(){
+    const RewardNFT = await hre.ethers.getContractFactory("RewardNFT");
+    const rewardNFTContract = await RewardNFT.deploy();
+
+    await rewardNFTContract.deployed();
+    console.log("Reward NFT contract address:", rewardNFTContract.address);
+
+    saveRewardFEFiles(rewardNFTContract);
+}
+
+function saveRewardFEFiles(contract) {
+    const fs = require("fs");
+    const contractsDir = __dirname + "/../client/src/abi";
+
+    if (!fs.existsSync(contractsDir)) {
+        fs.mkdirSync(contractsDir);
+    }
+
+    fs.writeFileSync(
+        contractsDir + "/contract-address.json",
+        JSON.stringify({ RewardNFT: contract.address }, undefined, 2)
+    );
+
+    const RewardNFTArtifact = artifacts.readArtifactSync("RewardNFT");
+
+    fs.writeFileSync(
+        contractsDir + "/RewardNFT.json",
+        JSON.stringify(RewardNFTArtifact, null, 2)
     );
 }
 
